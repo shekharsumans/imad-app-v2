b9825bd9ffcd11e78f04334d270051af38e1bcdd
shekharsumans <shekharsuman62@gmail.com>
2017-02-19 11:52:45 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
+app.get('/articlee-one',function(req,res){
     res.sendFile(path.join(__dirname, '', 'article-one.html'));
 });
 
