f24e9f2c54cec936e2dab09421e5af5b2e4b9ebc
shekharsumans <shekharsuman62@gmail.com>
2017-02-19 21:09:00 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,40 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-function createTemplate(data){
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    var htmlTemplate =`
-  <html>
-   <head>
-    <title> ${title }</title>
-    <meta name="viewport" content="width=device-width, initiak-scalev=1" />
-      <link href="/ui/style.css" rel="stylesheet" />
-   </head>
 
-  <body>
-    <div class="container">
-      <div>
-      <a href= "/">Home</a>
-      </div>
-     <hr/>
-          <h3> ${heading}
-          </h3>
-      <div>
-          ${date}
-       </div>
-
-       <div>
-          ${content}
-       </div>
-    </div>
-    </body>
-   </html> 
-   `;
-   return htmlTemplate;
-}
 var articles =  {
    articleOne: {
     title: 'A1',
@@ -85,6 +52,40 @@ var articles =  {
           </p>`
 }
 };
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate =`
+    <html>
+     <head>
+    <title> ${title }</title>
+    <meta name="viewport" content="width=device-width, initiak-scalev=1" />
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+     <div class="container">
+      <div>
+      <a href= "/">Home</a>
+      </div>
+     <hr/>
+          <h3> ${heading}
+          </h3>
+      <div>
+          ${date}
+       </div>
+
+       <div>
+          ${content}
+       </div>
+     </div>
+     </body>
+     </html> 
+     `;
+     return htmlTemplate;
+}
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
