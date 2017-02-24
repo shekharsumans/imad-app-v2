7701c7f8bfbbb4dcfd9913aed5be9f090b8ead3a
shekharsumans <shekharsuman62@gmail.com>
2017-02-24 05:16:45 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('imad-app-v2/ui/IMG_20161029_164143250.jpg', function (req,res){
+app.get('/ui/IMG_20161029_164143250.jpg', function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'IMG_20161029_164143250.jpg'));
 })
 
