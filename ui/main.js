7623afe17d6510df97b5ed11248858485fad80b9
shekharsumans <shekharsuman62@gmail.com>
2017-02-25 22:58:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var elemnt = document.getElementById('main-text');
 elemnt.innerHTML = 'suman shekhar';
 //move the image
-var img = document.getElementById('img');
+var img = document.getElementById('img1');
 img.onclick = Function{
   img.style.marginleft = '100px';  
 };
