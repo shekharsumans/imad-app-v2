4deac395fff7ce7270c42de843707310833d3c98
shekharsumans <shekharsuman62@gmail.com>
2017-02-27 09:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ elemnt.innerHTML = 'suman shekhar';
 var img = document.getElementById('abc4');
 var marginLeft = 0;
 function moveRight() {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 30);
   };
